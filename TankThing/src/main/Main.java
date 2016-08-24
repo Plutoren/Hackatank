d87e6d33451721d547e36c5020d641cb356291e0
stefan-blair <ilfuzzyhead@gmail.com>
2016-08-24 11:08:33 -0400

    making sure git is working gucci

--- TankThing/src/main/Main.java
+++ TankThing/src/main/Main.java
@@ -3,9 +3,10 @@ package main;
 public class Main {
 	
 	public static void main(String[] args) {
-	
-		System.out.println("Hello World");
-	
+		
+		int i = 0;
+		System.out.println("Hello World: " + i );
+		
 	}
 	
 }
