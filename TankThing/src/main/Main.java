91ab01c0ecb5019638600f1494c1cb7580b3ee8f
stefan-blair <ilfuzzyhead@gmail.com>
2016-08-24 13:55:20 -0400

    initial network branch commit

--- TankThing/src/main/Main.java
+++ TankThing/src/main/Main.java
@@ -1,39 +1,8 @@
 package main;
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import javax.swing.*;     
 
 public class Main {
 	
 	public static void main(String[] args) {
-		
-		
-		
-		javax.swing.SwingUtilities.invokeLater(new Runnable() {
-		        @Override
-		        public void run() {
-		        createAndShowGUI();
-		    }
-		});
-		
-	}
 	
-		 private static void createAndShowGUI() {
-			 
-		        JFrame frame = new JFrame("TankThing");
-		        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		        JLabel emptyLabel = new JLabel();
-		        frame.add(new JLabel(new ImageIcon("/TankThing/tankSpriteGreen.png")));
-
-		        emptyLabel.setPreferredSize(new Dimension(700, 400));
-		       
-		        frame.getContentPane().add(emptyLabel, BorderLayout.PAGE_START);
-		        frame.pack();
-		        frame.setVisible(true);
-		    }
-
-		
-	
-	
-	
+	}	
 }
