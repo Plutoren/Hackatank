84d839690a6a30aca1b3fe1b53de44c39490f456
Plutoren <gordonsully@yahoo.com>
2016-08-24 13:03:15 -0400

    Added basic gui and text field for ip

--- TankThing/src/main/Main.java
+++ TankThing/src/main/Main.java
@@ -1,4 +1,7 @@
 package main;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import javax.swing.*;     
 
 public class Main {
 	
@@ -7,6 +10,31 @@ public class Main {
 		int i = 5;
 		System.out.println("Hello World: " + i );
 		
+		javax.swing.SwingUtilities.invokeLater(new Runnable() {
+		        @Override
+		        public void run() {
+		        createAndShowGUI();
+		    }
+		});
+		
 	}
+	
+		 private static void createAndShowGUI() {
+			 
+		        JFrame frame = new JFrame("TankThing");
+		        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		        JLabel emptyLabel = new JLabel();
+		        frame.add(new JLabel(new ImageIcon("/TankThing/tankSpriteGreen.png")));
+
+		        emptyLabel.setPreferredSize(new Dimension(700, 400));
+		       
+		        frame.getContentPane().add(emptyLabel, BorderLayout.PAGE_START);
+		        frame.pack();
+		        frame.setVisible(true);
+		    }
+
+		
+	
+	
 	
 }
